94eaaf0d588640ebad58d394ee10f9f582b12150
Vinu Sagar <vinusagar@live.in>
2020-03-20 07:30:57 +0530

    Model updated

--- src/main/java/com/notyfyd/model/RoleModel.java
+++ src/main/java/com/notyfyd/model/RoleModel.java
@@ -5,23 +5,16 @@ import java.util.List;
 public class RoleModel {
     private String name;
     private String description;
-
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
-
 }
